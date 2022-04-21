2dabbab9c3df099698f107a84295640b330a7c6b
Sourav <sourav.iit.nstu@gmail.com>
2022-04-21 16:42:19 +0600

    using multiple style

--- App.js
+++ App.js
@@ -16,9 +16,9 @@ return (
 export default function App() {
   return (
     <View style={styles.container}>
-      <Name style={{fontSize:20}} name="Sourav" age="29" ></Name>
-      <Text style={{fontSize:20}} onPress={()=>{console.log("click sourav")}} >click button</Text>
-      <Text numberOfLines={3} selectable>Flex Dimensions
+      <Name  name="Sourav" age="29" ></Name>
+      <Text style={textColor.baseText} onPress={()=>{console.log("click sourav")}} >click button</Text>
+      <Text numberOfLines={3} selectable style={[textColor.baseText,textColor.titleText,back.backColor]}>Flex Dimensions
        Use flex in a component's style to have the component expand and shrink dynamically based on available space. Normally you will use flex: 1, which tells a component to fill all available space, shared evenly amongst other components with the same parent. The larger the flex given, the higher the ratio of space a component will take compared to its siblings.</Text>
       <StatusBar style="auto" />
     </View>
@@ -32,4 +32,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+});
+
+const textColor = StyleSheet.create({
+  baseText: {
+    fontFamily: "Arial",
+    color:'red'
+  },
+  titleText: {
+    fontWeight: "bold"
+  }
+});
+
+
+const back = StyleSheet.create({
+    backColor: {
+      backgroundColor:'pink'
+    }
+
 });

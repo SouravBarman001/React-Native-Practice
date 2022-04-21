9f3c825b03cf6516d4bf4b3bd43ba5b2b2eb5bb5
Sourav <sourav.iit.nstu@gmail.com>
2022-04-21 14:56:46 +0600

    onPress,numberofLines,selectable uses

--- App.js
+++ App.js
@@ -16,7 +16,10 @@ return (
 export default function App() {
   return (
     <View style={styles.container}>
-      <Name name="Sourav" age="29"></Name>
+      <Name style={{fontSize:20}} name="Sourav" age="29" ></Name>
+      <Text style={{fontSize:20}} onPress={()=>{console.log("click sourav")}} >click button</Text>
+      <Text numberOfLines={3} selectable>Flex Dimensions
+       Use flex in a component's style to have the component expand and shrink dynamically based on available space. Normally you will use flex: 1, which tells a component to fill all available space, shared evenly amongst other components with the same parent. The larger the flex given, the higher the ratio of space a component will take compared to its siblings.</Text>
       <StatusBar style="auto" />
     </View>
   );
